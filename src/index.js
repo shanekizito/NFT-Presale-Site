2b6bd73da1ee7d55f67b29fb23a91c9b74ee133a
shanekizito <shaneyoungk@gmail.com>
2022-05-03 13:01:50 +0300

    .

--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-import Navigation from './Components/Navigation';
-
 
 
 ReactDOM.render(
   <React.StrictMode>
-      
     <App/>
-   
   </React.StrictMode>,
   document.getElementById('root')
 );
